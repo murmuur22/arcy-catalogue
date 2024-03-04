fde73cd78a466689d2658fc7ae381f0eb56ecfa9
murmuur22 <69942927+murmuurr@users.noreply.github.com>
2024-03-04 18:17:03 -0500

    added rotation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,36 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<script>
+    import { onMount } from "svelte";
 
-<style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.100);
+    const images = [
+        "/images/1.jpg",
+        "/images/2.jpg",
+        "/images/3.jpg",
+        "/images/4.jpg",
+        "/images/5.jpg",
+        "/images/6.jpg",
+        "/images/7.jpg",
+        "/images/8.jpg",
+    ];
+    $: curr_image = 0;
+
+    function rotate() {
+        if (curr_image + 1 >= images.length) {
+            curr_image = 0;
+            return
+        }
+        curr_image += 1;
     }
-</style>
+
+    onMount(() => {
+        setInterval(()=>{
+            rotate()
+        }, 500)
+    });
+
+</script>
+
+<div class="h-screen w-screen flex justify-center items-center">
+    {#key curr_image} 
+        <img src={images[curr_image]} class="h-5/6"/>
+    {/key}
+</div>
