0d77a5a0200022839c2659a0fcdf752b117368ea
murmuur22 <69942927+murmuurr@users.noreply.github.com>
2024-03-05 01:19:22 -0500

    testing enhanced image for downscaling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -60,21 +60,21 @@
 </script>
 
 {#if state == STATE.intro}
-<img src="/bg-left.png" class="absolute left-0 w-screen" alt="background">
-<img src="/bg-right.png" class="absoliute right-0 w-screen" alt="background">
+<enhanced:img src="/bg-left.png" class="absolute left-0 w-screen" alt="background">
+<enhanced:img src="/bg-right.png" class="absoliute right-0 w-screen" alt="background">
 <div class="w-full h-screen bg-white"
     use:inview={{ rootMargin: '80%' }}
     on:inview_enter={()=>{isInView = true;}}
     on:inview_leave={()=>{isInView = false;}}
 />
 <div out:fade={{duration: 800}} class="fixed left-0 top-0 h-screen w-screen flex justify-center items-center">
-    <img src={images[curr_image]} class="h-5/6 pointer-events-none" alt="rotator">
+    <enhanced:img src={images[curr_image]} class="h-5/6 pointer-events-none" alt="rotator">
 </div>
 {:else if state == STATE.outro}
 <div  id="vidWrapper" class="fixed left-0 top-0 h-screen w-screen flex justify-center items-center">
     <video in:fade={{duration: 200}} id="vid" autoplay muted src="/video/splash.mp4" class="pointer-events-none fixed h-[75%]" />
     <div id="vidBg" class="fixed opacity-0 bg-black h-screen w-screen" />
-    <img id="arcteryx" alt="gore-tex" src="/graphics/arcteryx.png" class="pointer-events-none opacity-0 fixed w-1/6"/>
-    <img id="goretex" alt="gore-tex" src="/graphics/gore-tex.png" class="pointer-events-none opacity-0 fixed w-1/6"/>
+    <enhanced:img id="arcteryx" alt="gore-tex" src="/graphics/arcteryx.png" class="pointer-events-none opacity-0 fixed w-1/6"/>
+    <enhanced:img id="goretex" alt="gore-tex" src="/graphics/gore-tex.png" class="pointer-events-none opacity-0 fixed w-1/6"/>
 </div>
 {/if}
