2c240bcf3397acf6b6f3804583afb175e3b8226e
murmuur22 <69942927+murmuurr@users.noreply.github.com>
2024-03-05 00:17:48 -0500

    added features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,20 @@
-<script>
+<script lang="ts">
+    import { gsap } from "gsap";
     import { onMount } from "svelte";
+    import { blur } from 'svelte/transition';
+    import { inview } from 'svelte-inview';
 
-    const images = [
-        "/images/1.jpg",
-        "/images/2.jpg",
-        "/images/3.jpg",
-        "/images/4.jpg",
-        "/images/5.jpg",
-        "/images/6.jpg",
-        "/images/7.jpg",
-        "/images/8.jpg",
+    const images: Array<string> = [
+        "/images/1.png",
+        "/images/2.png",
+        "/images/3.png",
+        "/images/4.png",
+        "/images/5.png",
+        "/images/6.png",
+        "/images/7.png",
+        "/images/8.png",
     ];
-    $: curr_image = 0;
+    $: curr_image = 0 satisfies number;
 
     function rotate() {
         if (curr_image + 1 >= images.length) {
@@ -21,16 +24,56 @@
         curr_image += 1;
     }
 
-    onMount(() => {
-        setInterval(()=>{
-            rotate()
-        }, 500)
-    });
+    enum STATE {
+        intro,
+        outro
+    };
+    let state = STATE.intro satisfies STATE;
+
+    let isInView: boolean = false;
+
+    let tick: number = 0;
+    let ticker: number = setInterval(()=>{
+        tick += 10;
+
+        if (tick % 500 == 0) rotate();
+
+        if (isInView) {
+            if (tick % 70 == 0) rotate();
+
+            setTimeout(() => {
+                state = STATE.outro;
+                let tl = gsap.timeline();
+                tl.to("#vidBg", {opacity: 1, duration: 0.8});
+                tl.to("#vid", {opacity: 0, duration: 2}, 3);
+                tl.to("#goretex", {opacity: 1, duration: 0.3}, "<");
+                tl.to("#goretex", {x: 250, duration: 1.5, ease: "power2.out"});
+                tl.to("#arcteryx", {x: -250, opacity: 1, duration: 1.5, ease: "power2.out"}, "<");
+                tl.to("#vidWrapper", {y: -2000, opacity: 0, duration: 1.5, ease: "expo"}, "+=0.3");
+
+                clearInterval(ticker);
+            }, 700);
+        }
+    },10);
 
 </script>
 
-<div class="h-screen w-screen flex justify-center items-center">
-    {#key curr_image} 
-        <img src={images[curr_image]} class="h-5/6"/>
-    {/key}
+{#if state == STATE.intro}
+<img src="/bg-left.png" class="absolute left-0 w-screen" alt="background">
+<img src="/bg-right.png" class="absoliute right-0 w-screen" alt="background">
+<div class="w-full h-screen bg-white"
+    use:inview={{ rootMargin: '50%' }}
+    on:inview_enter={()=>{isInView = true;}}
+    on:inview_leave={()=>{isInView = false;}}
+/>
+<div class="fixed left-0 top-0 h-screen w-screen flex justify-center items-center">
+    <img src={images[curr_image]} class="h-5/6 pointer-events-none" alt="rotator">
 </div>
+{:else if state == STATE.outro}
+<div id="vidWrapper" class="fixed left-0 top-0 h-screen w-screen flex justify-center items-center">
+    <div id="vidBg" class="fixed opacity-0 bg-black h-screen w-screen" />
+    <video id="vid" autoplay muted src="/video/splash.mp4" class="pointer-events-none fixed h-4/6" />
+    <img id="arcteryx" alt="gore-tex" src="/graphics/arcteryx.png" class="pointer-events-none opacity-0 fixed w-1/6"/>
+    <img id="goretex" alt="gore-tex" src="/graphics/gore-tex.png" class="pointer-events-none opacity-0 fixed w-1/6"/>
+</div>
+{/if}
