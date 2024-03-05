a46427dfbeed985f607fa60e1d237e43306f11d8
murmuur22 <69942927+murmuurr@users.noreply.github.com>
2024-03-05 01:14:22 -0500

    finished

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { gsap } from "gsap";
-    import { onMount } from "svelte";
-    import { blur } from 'svelte/transition';
+    import { fade, blur } from 'svelte/transition';
     import { inview } from 'svelte-inview';
 
     const images: Array<string> = [
@@ -39,20 +38,22 @@
         if (tick % 500 == 0) rotate();
 
         if (isInView) {
-            if (tick % 70 == 0) rotate();
+            //if (tick % 500 == 0) rotate();
 
-            setTimeout(() => {
-                state = STATE.outro;
-                let tl = gsap.timeline();
-                tl.to("#vidBg", {opacity: 1, duration: 0.8});
-                tl.to("#vid", {opacity: 0, duration: 2}, 3);
-                tl.to("#goretex", {opacity: 1, duration: 0.3}, "<");
-                tl.to("#goretex", {x: 250, duration: 1.5, ease: "power2.out"});
-                tl.to("#arcteryx", {x: -250, opacity: 1, duration: 1.5, ease: "power2.out"}, "<");
-                tl.to("#vidWrapper", {y: -2000, opacity: 0, duration: 1.5, ease: "expo"}, "+=0.3");
+            if (curr_image >= 6) {
+                setTimeout(() => {
+                    state = STATE.outro;
+                    let tl = gsap.timeline();
+                    tl.to("#vid", {opacity: 0, duration: 0.8}, 4);
+                    tl.to("#vidBg", {opacity: 1, duration: 0.8}, "<");
+                    tl.to("#goretex", {opacity: 1, duration: 1.3}, "<");
+                    tl.to("#goretex", {x: 300, duration: 1.5, ease: "power2.out"}, "+=0.4");
+                    tl.to("#arcteryx", {x: -300, opacity: 1, duration: 1.5, ease: "power2.out"}, "<");
+                    //tl.to("#vidWrapper", {y: -2000, opacity: 0, duration: 1.5, ease: "expo"}, "+=0.3");
 
-                clearInterval(ticker);
-            }, 700);
+                    clearInterval(ticker);
+                }, 1400);
+            }
         }
     },10);
 
@@ -62,17 +63,17 @@
 <img src="/bg-left.png" class="absolute left-0 w-screen" alt="background">
 <img src="/bg-right.png" class="absoliute right-0 w-screen" alt="background">
 <div class="w-full h-screen bg-white"
-    use:inview={{ rootMargin: '50%' }}
+    use:inview={{ rootMargin: '80%' }}
     on:inview_enter={()=>{isInView = true;}}
     on:inview_leave={()=>{isInView = false;}}
 />
-<div class="fixed left-0 top-0 h-screen w-screen flex justify-center items-center">
+<div out:fade={{duration: 800}} class="fixed left-0 top-0 h-screen w-screen flex justify-center items-center">
     <img src={images[curr_image]} class="h-5/6 pointer-events-none" alt="rotator">
 </div>
 {:else if state == STATE.outro}
-<div id="vidWrapper" class="fixed left-0 top-0 h-screen w-screen flex justify-center items-center">
+<div  id="vidWrapper" class="fixed left-0 top-0 h-screen w-screen flex justify-center items-center">
+    <video in:fade={{duration: 200}} id="vid" autoplay muted src="/video/splash.mp4" class="pointer-events-none fixed h-[75%]" />
     <div id="vidBg" class="fixed opacity-0 bg-black h-screen w-screen" />
-    <video id="vid" autoplay muted src="/video/splash.mp4" class="pointer-events-none fixed h-4/6" />
     <img id="arcteryx" alt="gore-tex" src="/graphics/arcteryx.png" class="pointer-events-none opacity-0 fixed w-1/6"/>
     <img id="goretex" alt="gore-tex" src="/graphics/gore-tex.png" class="pointer-events-none opacity-0 fixed w-1/6"/>
 </div>
