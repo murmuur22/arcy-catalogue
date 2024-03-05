51a0d9449bf0bd78f30257d4e691e5055746c134
murmuur22 <69942927+murmuurr@users.noreply.github.com>
2024-03-05 01:40:06 -0500

    added loading bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
+    import { Stretch } from 'svelte-loading-spinners';
+    import { onMount } from "svelte";
     import "../app.css";
+
+    let isReady: Boolean = false;
+
+    onMount(() => {
+        if (document.readyState === "complete") isReady = true;
+    });
 </script>
+
+
+{#if !isReady}
+    <div class="w-full h-full flex items-center justify-center">
+        <Stretch size="60" unit="px" duration="1s" color="#545454" />
+    </div>
+{/if}
 
 <slot />
